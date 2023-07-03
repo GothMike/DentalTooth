e39682bd83bceb0cc0268a9546b252b42f07f634
GothMike <117478074+GothMike@users.noreply.github.com>
2023-07-03 23:30:07 +0400

    Разработка слайдера
    - Разработка слайдера
    - Верстка секции "Отзывы" и адаптация ее под все устройства
    - Подключена библиотека Slick

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,17 @@ overlay.addEventListener("click", () => {
   menu.classList.remove("menu_active");
   overlay.classList.remove("header__overlay_active");
 });
+
+$(document).ready(function () {
+  $(".reviews__carousel").slick({
+    slidesToShow: 1,
+    speed: 1500,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 15000,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../img/svg/carousel/arrow-prev.svg" alt="prev"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../img/svg/carousel/arrow-next.svg" alt="next"></button>',
+  });
+});
